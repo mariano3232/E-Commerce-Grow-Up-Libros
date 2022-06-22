a695584aa7060d46b2e3778d025b081b6febc63d
guillobr <guillermobr88@gmail.com>
2022-06-22 11:22:31 -0300

    devel

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import "./App.css";
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -12,26 +11,11 @@ import FAQ from "./components/FAQ";
 import Landing from "./components/Landing";
 import Author from "./components/Author";
 import BookDetails from "./components/BookDetails";
-=======
-import './App.css';
-import React from 'react';
-import {BrowserRouter , Routes , Route} from 'react-router-dom';
-import { getBooks } from './actions';
-import { useDispatch , useSelector } from 'react-redux';
-import { useEffect } from 'react';
-import NavBar from './components/NavBar';
-import  Home  from './components/Home';
-import AboutUs from './components/AboutUs';
-import FAQ from './components/FAQ'
-import Landing from './components/Landing';
-import Author from './components/Author';
-import BookDetails from './components/BookDetails';
 import AddBook from './components/AddBook';
 import Add from './components/Add';
 import AddAuthor from './components/AddAuthor';
 import BottomBar from './components/BottomBar';
 
->>>>>>> guille
 
 function App() {
   const dispatch = useDispatch();
