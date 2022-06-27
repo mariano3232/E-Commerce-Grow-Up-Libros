5e9af4c5dc2dd234cf4725ef064c5cc0cb837d6d
Jean Pierre Huaman Gomez <jeanpier.dev@outlook.com>
2022-06-26 21:42:47 -0500

    styÃles putAuthor

--- client/src/components/PutAuthor.jsx
+++ client/src/components/PutAuthor.jsx
@@ -1,42 +1,40 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import style from '../Styles/PutAuthor.module.css'
+export default function PutAuthor() {
+  const allAuthors = useSelector((state) => state.authors)
+  const orderedAuthors = allAuthors.sort(function (a, b) {
+    if (a.name.toLowerCase() > b.name.toLowerCase()) {
+      return 1
+    }
+    if (b.name.toLowerCase() > a.name.toLowerCase()) {
+      return -1
+    }
+    return 0
+  })
 
-
-export default function PutAuthor(){
-
-    const allAuthors = useSelector( state => state.authors)
-    const orderedAuthors = allAuthors.sort(function (a, b) {
-        if (a.name.toLowerCase() > b.name.toLowerCase()) {
-          return 1;
-        }
-        if (b.name.toLowerCase() > a.name.toLowerCase()) {
-          return -1;
-        }
-        return 0;
-      })
-
-    return(
-
-        <div>
-            
-            {
-                orderedAuthors.map(author => {
-                    return(
-                        <div>
-                        <h5>{author.name} {author.surname} </h5>
-                        <Link to ={'/putAuthorID/' + author._id}>
-                        <button>Modificar</button>
-                        </Link>
-                        </div>
-                    )
-                })
-
-            }
-
-                <Link to="/admin">
-                <button>Administrador</button>
-            </Link>
-        </div>
-    )
+  return (
+    <div className={style.containerPutList}>
+      <h1>Autores</h1>
+      <div className={style.grid}>
+        {orderedAuthors.map((author) => {
+          return (
+            <div className={style.cardItem}>
+              <h5>
+                {author.name} {author.surname}
+              </h5>
+              <img src={author.picture} alt='' />
+              <Link to={'/putAuthorID/' + author._id}>
+                <button className={style.btn}>Modificar</button>
+              </Link>
+            </div>
+          )
+        })}
+      </div>
+      <Link to='/admin'>
+        <button>Administrador</button>
+      </Link>
+    </div>
+  )
 }
