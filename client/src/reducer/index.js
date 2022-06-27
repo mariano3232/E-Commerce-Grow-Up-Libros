48af56e731514613c5a14cb29a58ca004d150f9b
guillobr <guillermobr88@gmail.com>
2022-06-27 10:25:53 -0300

    order

--- client/src/reducer/index.js
+++ client/src/reducer/index.js
@@ -14,85 +14,92 @@ function rootReducer(state = initialState, action) {
   switch (action.type) {
       
     case "GET_BOOKS":
-      
-      const allBooks = action.payload;
-      let booksOrder =
-        action.genres === "All"
-          ? allBooks
-          : allBooks.filter((element) =>
-              element.genres.find((e) => e.genres === action.genres)
-            );
-      
-      if (action.price !== undefined) {
-        booksOrder =
-          action.price === "Asc"
-            ? booksOrder.sort(function (a, b) {
-                if (a.price > b.price) {
-                  return 1;
-                }
-                if (b.price > a.price) {
-                  return -1;
-                }
-                return 0;
-              })
-            : booksOrder.sort(function (a, b) {
-                if (a.price > b.price) {
-                  return -1;
-                }
-                if (b.price > a.price) {
-                  return 1;
-                }
-                return 0;
-              });
-      } else if (action.title !== undefined) {
-        booksOrder =
-          action.title === "Asc"
-            ? booksOrder.sort(function (a, b) {
-                if (a.title.toLowerCase() > b.title.toLowerCase()) {
-                  return 1;
-                }
-                if (b.title.toLowerCase() > a.title.toLowerCase()) {
-                  return -1;
-                }
-                return 0;
-              })
-            : booksOrder.sort(function (a, b) {
-                if (a.title.toLowerCase() > b.title.toLowerCase()) {
-                  return -1;
-                }
-                if (b.title.toLowerCase() > a.title.toLowerCase()) {
-                  return 1;
-                }
-                return 0;
-              });
-      } /* else {
-        action.rating === "Asc"
-          ? rating.sort(function (a, b) {
-              if (a.rating > b.rating) {
-                return 1;
-              }
-              if (b.rating > a.rating) {
-                return -1;
-              }
-              return 0;
-            })
-          : rating.sort(function (a, b) {
-              if (a.rating > b.rating) {
-                return -1;
-              }
-              if (b.rating > a.rating) {
-                return 1;
-              }
-              return 0;
-            });
-      } */
-      
       return {
         ...state,
-        books: booksOrder,
-        booksCopy: action.payload,
-        booksTop: action.payload
-      };
+        books: action.payload,
+        booksCopy: action.payload
+    }   
+
+
+      
+      // const allBooks = action.payload;
+      // let booksOrder =
+      //   action.genres === "All"
+      //     ? allBooks
+      //     : allBooks.filter((element) =>
+      //         element.genres.find((e) => e.genres === action.genres)
+      //       );
+      
+      // if (action.price !== undefined) {
+      //   booksOrder =
+      //     action.price === "Asc"
+      //       ? booksOrder.sort(function (a, b) {
+      //           if (a.price > b.price) {
+      //             return 1;
+      //           }
+      //           if (b.price > a.price) {
+      //             return -1;
+      //           }
+      //           return 0;
+      //         })
+      //       : booksOrder.sort(function (a, b) {
+      //           if (a.price > b.price) {
+      //             return -1;
+      //           }
+      //           if (b.price > a.price) {
+      //             return 1;
+      //           }
+      //           return 0;
+      //         });
+      // } else if (action.title !== undefined) {
+      //   booksOrder =
+      //     action.title === "Asc"
+      //       ? booksOrder.sort(function (a, b) {
+      //           if (a.title.toLowerCase() > b.title.toLowerCase()) {
+      //             return 1;
+      //           }
+      //           if (b.title.toLowerCase() > a.title.toLowerCase()) {
+      //             return -1;
+      //           }
+      //           return 0;
+      //         })
+      //       : booksOrder.sort(function (a, b) {
+      //           if (a.title.toLowerCase() > b.title.toLowerCase()) {
+      //             return -1;
+      //           }
+      //           if (b.title.toLowerCase() > a.title.toLowerCase()) {
+      //             return 1;
+      //           }
+      //           return 0;
+      //         });
+      // } /* else {
+      //   action.rating === "Asc"
+      //     ? rating.sort(function (a, b) {
+      //         if (a.rating > b.rating) {
+      //           return 1;
+      //         }
+      //         if (b.rating > a.rating) {
+      //           return -1;
+      //         }
+      //         return 0;
+      //       })
+      //     : rating.sort(function (a, b) {
+      //         if (a.rating > b.rating) {
+      //           return -1;
+      //         }
+      //         if (b.rating > a.rating) {
+      //           return 1;
+      //         }
+      //         return 0;
+      //       });
+      // } */
+      
+      // return {
+      //   ...state,
+      //   books: booksOrder,
+      //   booksCopy: action.payload,
+      //   booksTop: action.payload
+      // };
       
       
    case 'GET_BOOK_TITLE':
