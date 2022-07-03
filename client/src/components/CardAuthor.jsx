ef9559bf9e878b2fac5f96148801a3b5de9bd166
Jean Pierre Huaman Gomez <jeanpier.dev@outlook.com>
2022-07-03 10:58:14 -0500

    styles ux

--- client/src/components/CardAuthor.jsx
+++ client/src/components/CardAuthor.jsx
@@ -1,16 +1,18 @@
+import React from 'react'
+import { useState } from 'react'
+import styles from '../Styles/cardAuthor.module.css'
 
-import React from 'react';
-import { useState } from 'react';
-import styles from '../Styles/cardAuthor.module.css';
-
-const CardAuthor = ({name, surname, picture}) => {
-    const defaultImg='https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png'
-    return (
-        <div className={styles.container}>
-            <h3 className={styles.name}>{name} {surname} </h3>
-            <img src={picture}  className={styles.img} alt='Buscando imagen...'/>
-        </div>
-    )
+const CardAuthor = ({ name, surname, picture }) => {
+  const defaultImg =
+    'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png'
+  return (
+    <div className={styles.container}>
+      <h3 className={styles.name}>
+        {name} {surname}{' '}
+      </h3>
+      <img src={picture} className={styles.img} alt='Buscando imagen...' />
+    </div>
+  )
 }
 
-export default CardAuthor;
+export default CardAuthor
