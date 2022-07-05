14f681b6635838474bf341af63ea9e6390eaf30a
guillobr <guillermobr88@gmail.com>
2022-07-05 16:06:45 -0300

    nuevos permisos de administrador, rutas banneadas, y orden en el codigo en carpeta adminPro

--- client/src/components/Admin/Admin.jsx
+++ client/src/components/Admin/Admin.jsx
@@ -2,12 +2,18 @@ import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import styles from '../../Styles/DashboardAdmin.module.css'
 import { getBooksAdmin , getAuthorsAdmin } from '../../actions'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 
 export function Admin() {
 
   const dispatch = useDispatch()
+
+  //const usuarios = useSelector(state=>state.users)
+  const usuario = useSelector(state=>state.userLogged)
+  //console.log('U:',usuarios)
+ 
+  
 
   useEffect(()=>{
     dispatch(getBooksAdmin())
@@ -20,37 +26,57 @@ export function Admin() {
   return (
     <div className={styles.admin}>
       <div className={styles.containerAdmin}>
+
+        {usuario[0].isAdminData === true ?
         <Link to='/add'>
           <button className={styles.btn}>Agregar Data</button>
         </Link>
+        : ''}
 
+        {usuario[0].isAdminData === true ? 
         <Link to='/delete'>
           <button className={styles.btn}>Borrar/Ocultar Data</button>
         </Link>
+        : ''}
 
+
+        {usuario[0].isAdminData === true ?
         <Link to='/put'>
           <button className={styles.btn}>Modificar Data</button>
         </Link>
+        : ''}
 
+
+        {usuario[0].isAdminStock=== true ?
         <Link to='/stock'>
           <button className={styles.btn}>Ver Stock</button>
         </Link>
+        : ''}
 
         {/* <Link to='/adminusers'>
           <button className={styles.btn}>Usuarios</button>
         </Link> */}
 
+
+{       usuario[0].isAdminUsers === true ?
         <Link to='/adminusers2'>
           <button className={styles.btn}>Usuarios</button>
         </Link>
+        : ''}
 
+
+        {usuario[0].isAdminOrders === true ?
         <Link to='/adminorders'>
           <button className={styles.btn}>Ordenes</button>
         </Link>
+        : ''}
 
+
+        {usuario[0].isAdminMarketing === true ?
         <Link to='/admincarrusel'>
           <button className={styles.btn}>Carrusel</button>
         </Link>
+        : ''}
         
       </div>
     </div>
