4d4de0c258ff80628764f96b82018412bf4318c6
Jean Pierre Huaman Gomez <jeanpier.dev@outlook.com>
2022-07-11 18:27:28 -0500

    customize alerts

--- client/src/components/CardBook.jsx
+++ client/src/components/CardBook.jsx
@@ -1,71 +1,69 @@
-import React, { useState } from "react";
-import styles from "../Styles/CardBook.module.css";
-import { Link } from "react-router-dom";
-import Fav from "./Fav";
-import { useDispatch, useSelector } from "react-redux";
-import { addToCart, purchaseOrder, putRating, updateAmount } from "../actions";
-import { Rating } from "@mui/material";
-import { useEffect } from "react";
-import { useAuth0 } from "@auth0/auth0-react";
-import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
-import Alert from "../functions/Alert";
+import React, { useState } from 'react'
+import styles from '../Styles/CardBook.module.css'
+import { Link } from 'react-router-dom'
+import Fav from './Fav'
+import { useDispatch, useSelector } from 'react-redux'
+import { addToCart, purchaseOrder, putRating, updateAmount } from '../actions'
+import { Rating } from '@mui/material'
+import { useEffect } from 'react'
+import { useAuth0 } from '@auth0/auth0-react'
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart'
+import Alert from '../functions/Alert'
 
 export default function CardBook({ title, cover, price, rating, id, stock }) {
-
   const dispatch = useDispatch()
   const { userLogged } = useSelector((state) => state)
-  console.log('usuarioLogueado:',userLogged)
+  console.log('usuarioLogueado:', userLogged)
   const productsAmount = useSelector((state) => state.cartAmount)
   const products = useSelector((state) => state.cart)
-  const myFavsBooks = useSelector(state=>state.userLoggedFavsBooksShowed)
-  const myFavsBooksIds = myFavsBooks.map(book=>book._id)
+  const myFavsBooks = useSelector((state) => state.userLoggedFavsBooksShowed)
+  const myFavsBooksIds = myFavsBooks.map((book) => book._id)
 
-
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithRedirect } = useAuth0()
 
   //const [ifRating, setIfRating] = useState();
-  const ifRating = changeRating(id);
-  const isBuy = changeBuy(id);
+  const ifRating = changeRating(id)
+  const isBuy = changeBuy(id)
 
   function changeBuy(id) {
     if (userLogged.length > 0 && userLogged[0].buyBooks.length > 0) {
-      let result = userLogged[0].buyBooks.indexOf(id);
+      let result = userLogged[0].buyBooks.indexOf(id)
       if (result === -1) {
-        return false;
+        return false
       } else {
-        return true;
+        return true
       }
     }
     if (userLogged.length === 0) {
-      return true;
+      return true
     }
   }
 
   function changeRating(id) {
     if (userLogged.length > 0 && userLogged[0].ratingBooks.length > 0) {
-      let result = userLogged[0].ratingBooks.indexOf(id);
+      let result = userLogged[0].ratingBooks.indexOf(id)
       if (result === -1) {
-        return false;
+        return false
       } else {
-        return true;
+        return true
       }
     }
     if (userLogged.length === 0) {
-      return true;
+      return true
     }
   }
 
   function handleRating(event, value) {
-    dispatch(putRating(id, value, userLogged[0]._id));
+    dispatch(putRating(id, value, userLogged[0]._id))
     // setIfRating(true);
   }
 
   function handleAddToCart(e) {
-    e.preventDefault();
-    if (userLogged.length === 0) return loginWithRedirect();
-    dispatch(addToCart(id));
-    dispatch(updateAmount(productsAmount + 1));
-    Alert("Libro agregado al carrito!", "success");
+    e.preventDefault()
+    if (userLogged.length === 0) return loginWithRedirect()
+    dispatch(addToCart(id))
+    dispatch(updateAmount(productsAmount + 1))
+    Alert('Libro agregado al carrito!', 'cart')
     setTimeout(function () {
       dispatch(
         purchaseOrder({
@@ -75,43 +73,45 @@ export default function CardBook({ title, cover, price, rating, id, stock }) {
           unit_price: products[products.length - 1].price,
           quantity: products[products.length - 1].amount,
         })
-      );
-    }, 200);
+      )
+    }, 200)
   }
 
   return (
     <div className={styles.container}>
       <div className={styles.image}>
-        <Link to={"/book/" + id}>
+        <Link to={'/book/' + id}>
           <img
             className={styles.img}
             src={cover}
-            alt="Not Found ):"
-            width="200x"
-            height="300"
+            alt='Not Found ):'
+            width='200x'
+            height='300'
           />
         </Link>
-        {userLogged?
-        <span
-          className={
-            isBuy ? styles.comprado + " " + styles.show : styles.comprado
-          }
-        >
-          COMPRADO
-        </span>
-        : <span
-        className={
-          isBuy ? styles.comprado + " " + styles.show : styles.comprado
-        }
-      >
-        NOOOO
-      </span>}
+        {userLogged ? (
+          <span
+            className={
+              isBuy ? styles.comprado + ' ' + styles.show : styles.comprado
+            }
+          >
+            COMPRADO
+          </span>
+        ) : (
+          <span
+            className={
+              isBuy ? styles.comprado + ' ' + styles.show : styles.comprado
+            }
+          >
+            NOOOO
+          </span>
+        )}
       </div>
       <div className={styles.block}>
         <div className={styles.rating}>
           {ifRating ? (
             <Rating
-              name="half-rating"
+              name='half-rating'
               value={rating}
               precision={0.5}
               onChange={(event, value) => handleRating(event, value)}
@@ -119,7 +119,7 @@ export default function CardBook({ title, cover, price, rating, id, stock }) {
             />
           ) : (
             <Rating
-              name="half-rating"
+              name='half-rating'
               value={0}
               precision={0.5}
               onChange={(event, value) => handleRating(event, value)}
@@ -134,7 +134,7 @@ export default function CardBook({ title, cover, price, rating, id, stock }) {
               <Fav
                 book={id}
                 painted={`${
-                  myFavsBooksIds.includes(id) ? "secondary" : "disabled"
+                  myFavsBooksIds.includes(id) ? 'secondary' : 'disabled'
                 }`}
               />
             </span>
@@ -144,19 +144,19 @@ export default function CardBook({ title, cover, price, rating, id, stock }) {
           <div>
             {stock > 1 ? (
               <AddShoppingCartIcon
-                cursor="pointer"
-                color="action"
-                fontSize="large"
+                cursor='pointer'
+                color='action'
+                fontSize='large'
                 onClick={(e) => handleAddToCart(e)}
               />
             ) : (
-              ""
+              ''
             )}
           </div>
         </div>
       </div>
     </div>
-  );
+  )
 }
 
 /* 
--- client/src/components/Fav.jsx
+++ client/src/components/Fav.jsx
@@ -1,45 +1,44 @@
-import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useAuth0 } from "@auth0/auth0-react";
-import { addFav, getUsers } from "../actions";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import Alert from "../functions/Alert";
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useAuth0 } from '@auth0/auth0-react'
+import { addFav, getUsers } from '../actions'
+import FavoriteIcon from '@mui/icons-material/Favorite'
+import Alert from '../functions/Alert'
 
 const Fav = ({ book, painted }) => {
-  console.log("painted<:", painted);
+  console.log('painted<:', painted)
 
+  const isLogged = useSelector((state) => state.userLogged)
 
-  const isLogged = useSelector((state) => state.userLogged);
+  const uBooksFav = useSelector((state) => state.userLoggedFavsBooksShowed)
 
-  const uBooksFav = useSelector((state) => state.userLoggedFavsBooksShowed);
-
-  const bookAdded = uBooksFav.filter((e) => e._id === book);
+  const bookAdded = uBooksFav.filter((e) => e._id === book)
 
   // const [state, setState] = useState('disabled')
 
-  const dispatch = useDispatch();
-  const { loginWithRedirect } = useAuth0();
+  const dispatch = useDispatch()
+  const { loginWithRedirect } = useAuth0()
 
   const handleClick = () => {
-    if (isLogged.length === 0) return loginWithRedirect();
-    if (bookAdded.length) return alert("Ya es un libro favorito");
-    const id = isLogged[0]._id;
-    dispatch(addFav(book, id));
-    Alert("Libro agregado a favoritos", "success");
-    dispatch(getUsers());
+    if (isLogged.length === 0) return loginWithRedirect()
+    if (bookAdded.length) return Alert('Ya es un libro favorito', 'fav')
+    const id = isLogged[0]._id
+    dispatch(addFav(book, id))
+    Alert('Libro agregado a favoritos', 'fav')
+    dispatch(getUsers())
     // setState('secondary')
-  };
+  }
 
   return (
     <div>
       <FavoriteIcon
-        cursor="pointer"
+        cursor='pointer'
         color={painted}
         sx={{ fontSize: 40 }}
         onClick={() => handleClick()}
       />
     </div>
-  );
-};
+  )
+}
 
-export default Fav;
+export default Fav
