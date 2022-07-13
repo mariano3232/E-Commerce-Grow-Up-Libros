91074e2b9de7d02fd60d3394aec26a63c81c1bdb
Jean Pierre Huaman Gomez <jeanpier.dev@outlook.com>
2022-07-13 16:30:08 -0500

    fix

--- client/src/components/UserSeguimientoOrden.jsx
+++ client/src/components/UserSeguimientoOrden.jsx
@@ -20,24 +20,23 @@ const UserSeguimientoOrden = () => {
           <h1>Detalles de tu Compra</h1>
           <p>Orden Id: {userOrders.pop()?._id}</p>
 
-          {userOrders.pop()?.usuario[0].email}
+          {userOrders.pop().usuario[0].email}
 
-          <p>Fecha: {userOrders.pop()?.fecha}</p>
-          <p>Total precio : {userOrders.pop()?.total}</p>
+          <p>Fecha: {userOrders.pop().fecha}</p>
+          <p>Total precio : {userOrders.pop().total}</p>
           <p>
             Producto:
-            {userOrders.pop()?.produt.map((product) => (
+            {userOrders.pop().produt.map((product) => (
               <li>{product}</li>
             ))}
           </p>
-          <p>Cantidad: {userOrders.pop()?.quantity}</p>
-          <p>Direccion: {userOrders.pop()?.usuario[0].address}</p>
-          <p>Codigo Postal: {userOrders.pop()?.usuario[0].postal}</p>
-          <p>Ciudad: {userOrders.pop()?.usuario[0].ciudad}</p>
-          <p>Pais: {userOrders.pop()?.usuario[0].country}</p>
-          <p>Telefono: {userOrders.pop()?.usuario[0].phone}</p>
-          <p>Estado de Pago: {userOrders.pop()?.status}</p>
-          <h1>Estado de tu compra: {userOrders.pop()?.status_order}</h1>
+          <p>Direccion: {userOrders.pop().usuario[0].address}</p>
+          <p>Codigo Postal: {userOrders.pop().usuario[0].postal}</p>
+          <p>Ciudad: {userOrders.pop().usuario[0].ciudad}</p>
+          <p>Pais: {userOrders.pop().usuario[0].country}</p>
+          <p>Telefono: {userOrders.pop().usuario[0].phone}</p>
+          <p>Estado de Pago: {userOrders.pop().status}</p>
+          <h1>Estado de tu compra: {userOrders.pop().status_order}</h1>
         </div>
       ) : (
         <p>No tienes ordenes</p>
