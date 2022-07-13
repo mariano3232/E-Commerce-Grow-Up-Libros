284711f4bd27e6f956359bef45696a8dd74a7016
Jean Pierre Huaman Gomez <jeanpier.dev@outlook.com>
2022-07-13 12:21:51 -0500

    fix segui

--- client/src/components/UserSeguimientoOrden.jsx
+++ client/src/components/UserSeguimientoOrden.jsx
@@ -11,40 +11,32 @@ const UserSeguimientoOrden = () => {
   const userOrders = ordersWithUsers.filter(
     (order) => order.usuario[0]._id === logged[0]._id
   )
-  console.log('SOY userOrders', userOrders)
-
-  const ultimaOrden = []
-
-  if (userOrders.length !== 0) {
-    ultimaOrden = userOrders.pop()
-  }
-  console.log('SOY ultimaOrden', ultimaOrden)
 
   return (
     <div>
-      {ultimaOrden ? (
+      {userOrders ? (
         <div>
           <h1>Detalles de Orden</h1>
-          <p>Orden Id: {ultimaOrden._id}</p>
+          <p>Orden Id: {userOrders.pop()._id}</p>
 
-          {ultimaOrden.usuario[0].email}
+          {userOrders.pop().usuario[0].email}
 
-          <p>Fecha: {ultimaOrden.fecha}</p>
-          <p>Total precio : {ultimaOrden.total}</p>
+          <p>Fecha: {userOrders.pop().fecha}</p>
+          <p>Total precio : {userOrders.pop().total}</p>
           <p>
             Producto:
-            {ultimaOrden.produt.map((product) => (
+            {userOrders.pop().produt.map((product) => (
               <li>{product}</li>
             ))}
           </p>
-          <p>Cantidad: {ultimaOrden.quantity}</p>
-          <p>Direccion: {ultimaOrden.usuario[0].address}</p>
-          <p>Codigo Postal: {ultimaOrden.usuario[0].postal}</p>
-          <p>Ciudad: {ultimaOrden.usuario[0].ciudad}</p>
-          <p>Pais: {ultimaOrden.usuario[0].country}</p>
-          <p>Telefono: {ultimaOrden.usuario[0].phone}</p>
-          <p>Estado de Pago: {ultimaOrden.status}</p>
-          <h1>Estado de Orden: {ultimaOrden.status_order}</h1>
+          <p>Cantidad: {userOrders.pop().quantity}</p>
+          <p>Direccion: {userOrders.pop().usuario[0].address}</p>
+          <p>Codigo Postal: {userOrders.pop().usuario[0].postal}</p>
+          <p>Ciudad: {userOrders.pop().usuario[0].ciudad}</p>
+          <p>Pais: {userOrders.pop().usuario[0].country}</p>
+          <p>Telefono: {userOrders.pop().usuario[0].phone}</p>
+          <p>Estado de Pago: {userOrders.pop().status}</p>
+          <h1>Estado de Orden: {userOrders.pop().status_order}</h1>
         </div>
       ) : (
         <p>No tienes ordenes</p>
