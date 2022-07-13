66b950b5e93874410a076edae806085cebdcb9c0
Jean Pierre Huaman Gomez <jeanpier.dev@outlook.com>
2022-07-13 12:04:31 -0500

    fix user seguimiento de compra

--- client/src/components/UserSeguimientoOrden.jsx
+++ client/src/components/UserSeguimientoOrden.jsx
@@ -5,37 +5,46 @@ const UserSeguimientoOrden = () => {
   const dispatch = useDispatch()
   const allOrders = useSelector((state) => state.orders)
   const logged = useSelector((state) => state.userLogged)
+
   const ordersWithUsers = allOrders.filter((order) => order.usuario.length > 0)
+
   const userOrders = ordersWithUsers.filter(
     (order) => order.usuario[0]._id === logged[0]._id
   )
-  const ultimaOrden = userOrders.pop()
+  const ultimaOrden = []
+  if (userOrders.length === 0) {
+    ultimaOrden = userOrders.pop()
+  }
 
   return (
     <div>
-      <h1>Detalles de Orden</h1>
-      <div>
-        <p>Orden Id: {ultimaOrden._id}</p>
+      {ultimaOrden.length === 0 ? (
+        <div>
+          <h1>Detalles de Orden</h1>
+          <p>Orden Id: {ultimaOrden._id}</p>
 
-        {ultimaOrden.usuario[0].email}
+          {ultimaOrden.usuario[0].email}
 
-        <p>Fecha: {ultimaOrden.fecha}</p>
-        <p>Total precio : {ultimaOrden.total}</p>
-        <p>
-          Producto:
-          {ultimaOrden.produt.map((product) => (
-            <li>{product}</li>
-          ))}
-        </p>
-        <p>Cantidad: {ultimaOrden.quantity}</p>
-        <p>Direccion: {ultimaOrden.usuario[0].address}</p>
-        <p>Codigo Postal: {ultimaOrden.usuario[0].postal}</p>
-        <p>Ciudad: {ultimaOrden.usuario[0].ciudad}</p>
-        <p>Pais: {ultimaOrden.usuario[0].country}</p>
-        <p>Telefono: {ultimaOrden.usuario[0].phone}</p>
-        <p>Estado de Pago: {ultimaOrden.status}</p>
-        <h1>Estado de Orden: {ultimaOrden.status_order}</h1>
-      </div>
+          <p>Fecha: {ultimaOrden.fecha}</p>
+          <p>Total precio : {ultimaOrden.total}</p>
+          <p>
+            Producto:
+            {ultimaOrden.produt.map((product) => (
+              <li>{product}</li>
+            ))}
+          </p>
+          <p>Cantidad: {ultimaOrden.quantity}</p>
+          <p>Direccion: {ultimaOrden.usuario[0].address}</p>
+          <p>Codigo Postal: {ultimaOrden.usuario[0].postal}</p>
+          <p>Ciudad: {ultimaOrden.usuario[0].ciudad}</p>
+          <p>Pais: {ultimaOrden.usuario[0].country}</p>
+          <p>Telefono: {ultimaOrden.usuario[0].phone}</p>
+          <p>Estado de Pago: {ultimaOrden.status}</p>
+          <h1>Estado de Orden: {ultimaOrden.status_order}</h1>
+        </div>
+      ) : (
+        <p>No tienes ordenes</p>
+      )}
     </div>
   )
 }
