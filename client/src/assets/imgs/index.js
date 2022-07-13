b2b203a7db6782a565ebfa444c805a73117c66af
Jean Pierre Huaman Gomez <jeanpier.dev@outlook.com>
2022-07-13 15:59:03 -0500

    refix

--- client/src/assets/imgs/index.js
+++ client/src/assets/imgs/index.js
@@ -1,8 +1,24 @@
 import logoBook from './logoBooks.png'
 import mercadoPago from './mercadopago.png'
 import email from './email.png'
+import books from './books.png'
+import carrito from './carrito.png'
+import deleteAccount from './delete-account.png'
+import errorBook from './errorBook.png'
+import updateUser from './updateUser.png'
+import favorito from './favorito.png'
+import imagesFiles from './image-files.png'
+import orderIcon from './order.png'
 export default {
   logoBook,
   mercadoPago,
   email,
+  books,
+  carrito,
+  deleteAccount,
+  errorBook,
+  updateUser,
+  favorito,
+  imagesFiles,
+  orderIcon,
 }
