96a7bb994551e211cb4e92fc7091052af70924e2
Jean Pierre Huaman Gomez <jeanpier.dev@outlook.com>
2022-07-13 16:52:27 -0500

    fix

--- client/src/components/UserSeguimientoOrden.jsx
+++ client/src/components/UserSeguimientoOrden.jsx
@@ -18,25 +18,32 @@ const UserSeguimientoOrden = () => {
       {userOrders.length !== 0 ? (
         <div>
           <h1>Detalles de tu Compra</h1>
-          <p>Orden Id: {userOrders.pop()?._id}</p>
+          <p>Orden Id: {userOrders[userOrders.length - 1]._id}</p>
 
-          {userOrders.pop().usuario[0].email}
+          {userOrders[userOrders.length - 1].usuario[0].email}
 
-          <p>Fecha: {userOrders.pop().fecha}</p>
-          <p>Total precio : {userOrders.pop().total}</p>
+          <p>Fecha: {userOrders[userOrders.length - 1].fecha}</p>
+          <p>Total precio : {userOrders[userOrders.length - 1].total}</p>
           <p>
             Producto:
-            {userOrders.pop().produt.map((product) => (
+            {userOrders[userOrders.length - 1].produt.map((product) => (
               <li>{product}</li>
             ))}
           </p>
-          <p>Direccion: {userOrders.pop().usuario[0].address}</p>
-          <p>Codigo Postal: {userOrders.pop().usuario[0].postal}</p>
-          <p>Ciudad: {userOrders.pop().usuario[0].ciudad}</p>
-          <p>Pais: {userOrders.pop().usuario[0].country}</p>
-          <p>Telefono: {userOrders.pop().usuario[0].phone}</p>
-          <p>Estado de Pago: {userOrders.pop().status}</p>
-          <h1>Estado de tu compra: {userOrders.pop().status_order}</h1>
+          <p>
+            Direccion: {userOrders[userOrders.length - 1].usuario[0].address}
+          </p>
+          <p>
+            Codigo Postal: {userOrders[userOrders.length - 1].usuario[0].postal}
+          </p>
+          <p>Ciudad: {userOrders[userOrders.length - 1].usuario[0].ciudad}</p>
+          <p>Pais: {userOrders[userOrders.length - 1].usuario[0].country}</p>
+          <p>Telefono: {userOrders[userOrders.length - 1].usuario[0].phone}</p>
+          <p>Estado de Pago: {userOrders[userOrders.length - 1].status}</p>
+          <h1>
+            Estado de tu compra:{' '}
+            {userOrders[userOrders.length - 1].status_order}
+          </h1>
         </div>
       ) : (
         <p>No tienes ordenes</p>
